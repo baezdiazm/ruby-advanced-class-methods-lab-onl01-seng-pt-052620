e554167e5e60afd8fbf2375fd7d4d18051056712
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-07-08 21:19:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,7 @@ class Song
     song = self.new
     song.name = (name.split(" - ")[1]).chomp(".mp3")
     song.artist_name = (name.split(" - ")[0])
-    @@all << song
+    #@@all << song
     song
   end
 
